441c1db8271ba9be957afc3b7f8c7355e109e5ab
jamest216 <james.ct216@gmail.com>
2019-03-25 15:10:06 -0500

    not done

---
+++ Ksu.Cis300.TrieLibrary/TrieWithNoChildren.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    class TrieWithNoChildren : ITrie
+    {
+        private bool _contains = false;
+        public ITrie Add(string s)
+        {
+            if(s == "")
+            {
+                _contains = true;
+            }
+            else
+            {
+                return new TrieWithOneChild(_contains, s);
+            }
+            return this;
+        }
+
+        public bool Contains(string s)
+        {
+            if(s != "")
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Ksu.Cis300.TrieLibrary/TrieWithOneChild.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    public class TrieWithOneChild : ITrie
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private bool _contains;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private ITrie _child;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private char _label;
+
+        public ITrie Add(string s)
+        {
+            if(s == "")
+            {
+                _contains = true;
+            }
+            return this;
+        }
+
+        public bool Contains(string s)
+        {
+            if(s == "")
+            {
+                return _contains;
+            }
+            return false;
+        }
+
+        public TrieWithOneChild(bool b, string s)
+        {
+            if (s == "" || (s[0] < 'a' || s[0] > 'z'))
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                _contains = b;
+                _label = s[0];
+                _child = new TrieWithNoChildren().Add(s.Substring(1));
+            }
+        }
+    }
+}
